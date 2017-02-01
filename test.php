e88b4dd8eb5fc9daa421110646a58b106fe90945
wapmorgan <wapmorgan@gmail.com>
2017-02-01 10:56:03 +0300

    Update test script

--- test.php
+++ test.php
@@ -2,9 +2,9 @@
 require __DIR__.'/vendor/autoload.php';
 
 //wapmorgan\TimeParser\TimeParser::enableDebug();
-$datetime = wapmorgan\TimeParser\TimeParser::parse('в 15:12:13 в следующий понедельник в следующем году в феврале через 15 часов через 10 минут через 11 секунд через 5 лет через 2 недели через 1 день через 10 месяцев', 'russian');
+$datetime = wapmorgan\TimeParser\TimeParser::parse('15 декабря 1977 года в 15:12:13 в следующий понедельник в следующем году в феврале через 15 часов через 10 минут через 11 секунд через 5 лет через 2 недели через 1 день через 10 месяцев', 'russian');
 var_dump($datetime->format('r'));
-$datetime = wapmorgan\TimeParser\TimeParser::parse('at 15:12:13 next monday next year in february in 15 hours in 10 minutes in 11 seconds in 5 years in 2 weeks in 1 day in 10 months', 'english');
+$datetime = wapmorgan\TimeParser\TimeParser::parse('15 december 1977 at 15:12:13 next monday next year in february in 15 hours in 10 minutes in 11 seconds in 5 years in 2 weeks in 1 day in 10 months', 'english');
 var_dump($datetime->format('r'));
 $datetime = wapmorgan\TimeParser\TimeParser::parse('15:12:13 prochaine lundi prochaine année février bout de 15 heures bout de 10 minutes bout de 11 secondes bout de 5 années bout de 2 semaines bout de 1 jour bout de 10 mois', 'french');
 var_dump($datetime->format('r'));
