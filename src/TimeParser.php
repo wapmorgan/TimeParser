1bbca9bb36d897ce5dc301166a34cd9ad4c14b3e
wapmorgan <wapmorgan@gmail.com>
2017-04-27 18:51:43 +0300

    Add tests and change structure of rules file

--- src/TimeParser.php
+++ src/TimeParser.php
@@ -1,9 +1,13 @@
 <?php
 namespace wapmorgan\TimeParser;
 
-use \DateTime;
+use DateTimeImmutable;
+use Exception;
 
 class TimeParser {
+	protected $languages;
+	protected $languagesRules = array();
+	protected $allowAlphabeticUnits = false;
 
 	static protected $months = array(
 		'january' => 1,
@@ -34,228 +38,262 @@ class TimeParser {
 		return self::$debug;
 	}
 
-	static public function parse($string, $languages = 'all', $allowAlphabeticUnits = true) {
-		$string = self::prepareString($string);
+	public function __construct($languages = null) {
+		if (!empty($languages))
+			$this->languages = $languages;
+		$this->populateLanguageRules();
+	}
 
+	public function allowAlphabeticUnits() {
+		$this->allowAlphabeticUnits = true;
+	}
+
+	public function disallowAlphabeticUnits() {
+		$this->allowAlphabeticUnits = false;
+	}
+
+	protected function populateLanguageRules() {
 		// collect rules
 		$available_languages = array_map(function ($lang) {
 			return strtolower(basename($lang, '.json'));
 		}, glob(dirname(__FILE__).'/../rules/*.json'));
-		if ($languages !== 'all') {
-			if (is_array($languages)) {
-				$available_languages = array_intersect($languages, $available_languages);
+
+		if ($this->languages !== 'all') {
+			if (is_array($this->languages)) {
+				$available_languages = array_intersect($this->languages, $available_languages);
 				if (!empty($available_languages)) {
 					DebugStream::show('Selected languages: '.implode(', ', $available_languages).PHP_EOL);
 				} else {
-					DebugStream::show('Parsing with default strtotime()'.PHP_EOL);
-					return self::parseWithStrtotime($string);
+					// DebugStream::show('Parsing with default strtotime()'.PHP_EOL);
+					throw new Exception('Unknown languages used: '.implode(',', $this->languages));
+					// return self::parseWithStrtotime($string);
 				}
 			} else {
-				if (in_array($languages, $available_languages)) {
-					$available_languages = array($languages);
-					DebugStream::show('Selected language: '.$languages.PHP_EOL);
+				if (in_array($this->languages, $available_languages)) {
+					$available_languages = array($this->languages);
+					DebugStream::show('Selected language: '.$this->languages.PHP_EOL);
 				} else {
-					DebugStream::show('Parsing with default strtotime()'.PHP_EOL);
-					return self::parseWithStrtotime($string);
+					// DebugStream::show('Parsing with default strtotime()'.PHP_EOL);
+					throw new Exception('Unknown language used: '.$this->languages);
+					// return self::parseWithStrtotime($string);
 				}
 			}
 		}
 
-		$datetime = new DateTime;
+		foreach ($available_languages as $language) {
+			$this->languagesRules[$language] = self::populateRules($language);
+		}
+	}
+
+	public function parse($string, $falseWhenNotChanged = false) {
+		$string = self::prepareString($string);
+		$datetime = $current_datetime = new DateTimeImmutable();
 
 		// apply rules
-		foreach ($available_languages as $language) {
-			$language = self::populateRules($language);
-			foreach ($language->rules as $rule) {
-				if (self::match($rule['regex'], $string, $matches)) {
-					DebugStream::show('Matched: '.$rule['regex'].PHP_EOL);
-					if ($rule['type'] == 'absolute') {
-						switch ($rule['name']) {
-							case 'date':
-								$month = $language->translateMonth($matches['month'][0]);
-								if (!empty($matches['year'][0]))
-									$year = $matches['year'][0];
-								else
-									$year = $datetime->format('Y');
-								if (!empty($matches['digit'][0])) {
-									$day = $matches['digit'][0];
-									DebugStream::show('Set date: '.$year.'-'.$month.'-'.$day.PHP_EOL);
-									$datetime->setDate($year, self::$months[$month], $day);
-								} else if ($allowAlphabeticUnits) {
-									$alpha = $language->translateUnit($matches['alpha'][0]);
-									if (is_numeric($alpha)) {
-										DebugStream::show('Set date: '.$year.'-'.$month.'-'.$alpha.PHP_EOL);
-										$datetime->setDate($year, self::$months[$month], $alpha);
+		foreach ($this->languagesRules as $language) {
+			foreach ($language->rules as $rule_type => $rules) {
+				foreach ($rules as $rule_name => $rule_regex) {
+					if (self::match($rule_regex, $string, $matches)) {
+						DebugStream::show('Matched: '.$rule_regex.PHP_EOL);
+						if ($rule_type == 'absolute') {
+							switch ($rule_name) {
+								case 'date':
+									$month = $language->translateMonth($matches['month'][0]);
+									if (!empty($matches['year'][0]))
+										$year = $matches['year'][0];
+									else
+										$year = $datetime->format('Y');
+									if (!empty($matches['digit'][0])) {
+										$day = $matches['digit'][0];
+										DebugStream::show('Set date: '.$year.'-'.$month.'-'.$day.PHP_EOL);
+										$datetime = $datetime->setDate($year, self::$months[$month], $day);
+									} else if ($this->allowAlphabeticUnits) {
+										$alpha = $language->translateUnit($matches['alpha'][0]);
+										if (is_numeric($alpha)) {
+											DebugStream::show('Set date: '.$year.'-'.$month.'-'.$alpha.PHP_EOL);
+											$datetime = $datetime->setDate($year, self::$months[$month], $alpha);
+										}
+										// parse here alphabetic value
 									}
-									// parse here alphabetic value
-								}
-								break;
-							case 'time':
-								if (!empty($matches['sec'])) {
-									$datetime->setTime($matches['hour'][0], $matches['min'][0], $matches['sec'][0]);
-									DebugStream::show('Set time: '.$matches['hour'][0].':'.$matches['min'][0].':'.$matches['sec'][0].PHP_EOL);
-								} else {
-									$datetime->setTime($matches['hour'][0], $matches['min'][0]);
-									DebugStream::show('Set time: '.$matches['hour'][0].':'.$matches['min'][0].PHP_EOL);
-								}
-								break;
-							case 'weekday':
-								if (!empty($matches['pronoun']) && ($pronoun = $language->translatePronoun($matches['pronoun'][0])) == 'next') {
-									$weekday = $language->translateWeekDay($matches['weekday'][0]);
-									$time = strtotime('next week '.$weekday);
-									DebugStream::show('Set weekday: next '.$weekday.PHP_EOL);
-								} else {
-									$weekday = $language->translateWeekDay($matches['weekday'][0]);
-									$time = strtotime('this week '.$weekday);
-									DebugStream::show('Set weekday: this '.$weekday.PHP_EOL);
-								}
-								$date = explode('.', date('d.m.Y', $time));
-								$datetime->setDate($date[2], $date[1], $date[0]);
-								break;
-							case 'year':
-								if (!empty($matches['pronoun'][0])) {
+									break;
+								case 'time':
+									if (!empty($matches['sec'])) {
+										$datetime = $datetime->setTime($matches['hour'][0], $matches['min'][0], $matches['sec'][0]);
+										DebugStream::show('Set time: '.$matches['hour'][0].':'.$matches['min'][0].':'.$matches['sec'][0].PHP_EOL);
+									} else {
+										$datetime = $datetime->setTime($matches['hour'][0], $matches['min'][0]);
+										DebugStream::show('Set time: '.$matches['hour'][0].':'.$matches['min'][0].PHP_EOL);
+									}
+									break;
+								case 'weekday':
+									if (!empty($matches['pronoun']) && ($pronoun = $language->translatePronoun($matches['pronoun'][0])) == 'next') {
+										$weekday = $language->translateWeekDay($matches['weekday'][0]);
+										$time = strtotime('next week '.$weekday);
+										DebugStream::show('Set weekday: next '.$weekday.PHP_EOL);
+									} else {
+										$weekday = $language->translateWeekDay($matches['weekday'][0]);
+										$time = strtotime('this week '.$weekday);
+										DebugStream::show('Set weekday: this '.$weekday.PHP_EOL);
+									}
+									$date = explode('.', date('d.m.Y', $time));
+									$datetime = $datetime->setDate($date[2], $date[1], $date[0]);
+									break;
+								case 'year':
+									if (!empty($matches['pronoun'][0])) {
+										$pronoun = $language->translatePronoun($matches['pronoun'][0]);
+										if ($pronoun == 'next') {
+											$datetime = $datetime->modify('+1 year');
+											DebugStream::show('Set year: +1'.PHP_EOL);
+										}
+									} else {
+										$year = $matches['digit'][0];
+										$datetime = $datetime->setDate($year, $datetime->format('m'), $datetime->format('d'));
+										DebugStream::show('Set year: '.$year.PHP_EOL);
+									}
+									break;
+								case 'month':
 									$pronoun = $language->translatePronoun($matches['pronoun'][0]);
 									if ($pronoun == 'next') {
-										$datetime->modify('+1 year');
-										DebugStream::show('Set year: +1'.PHP_EOL);
+										$datetime = $datetime->modify('+1 month');
+										DebugStream::show('Set month: +1'.PHP_EOL);
+									} else {
+										$month = $language->translateMonth($matches['month'][0]);
+										DebugStream::show('Set month: '.$month.PHP_EOL);
+										$datetime = $datetime->modify($month);
 									}
-								} else {
-									$year = $matches['digit'][0];
-									$datetime->setDate($year, $datetime->format('m'), $datetime->format('d'));
-									DebugStream::show('Set year: '.$year.PHP_EOL);
-								}
-								break;
-							case 'month':
-								$pronoun = $language->translatePronoun($matches['pronoun'][0]);
-								if ($pronoun == 'next') {
-									$datetime->modify('+1 month');
-									DebugStream::show('Set month: +1'.PHP_EOL);
-								} else {
-									$month = $language->translateMonth($matches['month'][0]);
-									DebugStream::show('Set month: '.$month.PHP_EOL);
-									$datetime->modify($month);
-								}
-								break;
-							case 'week':
-								$pronoun = $language->translatePronoun($matches['pronoun'][0]);
-								if ($pronoun == 'next') {
-									$datetime->modify('+1 week');
-									DebugStream::show('Set week: +1'.PHP_EOL);
-								}
-								break;
-						}
-					} else if ($rule['type'] == 'relative') {
-						switch ($rule['name']) {
-							case 'hour':
-								if (!empty($matches['digit'][0])) {
-									$hour = $matches['digit'][0];
-									$datetime->modify('+'.$hour.' hour');
-									DebugStream::show('Add offset: +'.$hour.' hour'.PHP_EOL);
-								} else if ($allowAlphabeticUnits) {
-									$alpha = $language->translateUnit($matches['alpha'][0]);
-									if (is_numeric($alpha)) {
-										DebugStream::show('Add offset: +'.$alpha.' hours'.PHP_EOL);
-										$datetime->modify('+'.$alpha.' hours');
+									break;
+								case 'week':
+									$pronoun = $language->translatePronoun($matches['pronoun'][0]);
+									if ($pronoun == 'next') {
+										$datetime = $datetime->modify('+1 week');
+										DebugStream::show('Set week: +1'.PHP_EOL);
 									}
-									// parse here alphabetic value
-								}
-								break;
+									break;
+							}
+						} else if ($rule_type == 'relative') {
+							switch ($rule_name) {
+								case 'hour':
+									if (!empty($matches['digit'][0])) {
+										$hour = $matches['digit'][0];
+										$datetime = $datetime->modify('+'.$hour.' hour');
+										DebugStream::show('Add offset: +'.$hour.' hour'.PHP_EOL);
+									} else if ($this->allowAlphabeticUnits) {
+										$alpha = $language->translateUnit($matches['alpha'][0]);
+										if (is_numeric($alpha)) {
+											DebugStream::show('Add offset: +'.$alpha.' hours'.PHP_EOL);
+											$datetime = $datetime->modify('+'.$alpha.' hours');
+										}
+										// parse here alphabetic value
+									}
+									break;
 
-							case 'minute':
-								if (!empty($matches['digit'][0])) {
-									$minute = $matches['digit'][0];
-									$datetime->modify('+'.$minute.' minute');
-									DebugStream::show('Add offset: +'.$minute.' minute'.PHP_EOL);
-								} else if ($allowAlphabeticUnits) {
-									$alpha = $language->translateUnit($matches['alpha'][0]);
-									if (is_numeric($alpha)) {
-										DebugStream::show('Add offset: +'.$alpha.' minutes'.PHP_EOL);
-										$datetime->modify('+'.$alpha.' minutes');
+								case 'minute':
+									if (!empty($matches['digit'][0])) {
+										$minute = $matches['digit'][0];
+										$datetime = $datetime->modify('+'.$minute.' minute');
+										DebugStream::show('Add offset: +'.$minute.' minute'.PHP_EOL);
+									} else if ($this->allowAlphabeticUnits) {
+										$alpha = $language->translateUnit($matches['alpha'][0]);
+										if (is_numeric($alpha)) {
+											DebugStream::show('Add offset: +'.$alpha.' minutes'.PHP_EOL);
+											$datetime = $datetime->modify('+'.$alpha.' minutes');
+										}
+										// parse here alphabetic value
 									}
-									// parse here alphabetic value
-								}
-								break;
+									break;
 
-							case 'sec':
-								if (!empty($matches['digit'][0])) {
-									$sec = $matches['digit'][0];
-									$datetime->modify('+'.$sec.' second');
-									DebugStream::show('Add offset: +'.$sec.' second'.PHP_EOL);
-								} else if ($allowAlphabeticUnits) {
-									$alpha = $language->translateUnit($matches['alpha'][0]);
-									if (is_numeric($alpha)) {
-										DebugStream::show('Add offset: +'.$alpha.' seconds'.PHP_EOL);
-										$datetime->modify('+'.$alpha.' seconds');
+								case 'sec':
+									if (!empty($matches['digit'][0])) {
+										$sec = $matches['digit'][0];
+										$datetime = $datetime->modify('+'.$sec.' second');
+										DebugStream::show('Add offset: +'.$sec.' second'.PHP_EOL);
+									} else if ($this->allowAlphabeticUnits) {
+										$alpha = $language->translateUnit($matches['alpha'][0]);
+										if (is_numeric($alpha)) {
+											DebugStream::show('Add offset: +'.$alpha.' seconds'.PHP_EOL);
+											$datetime = $datetime->modify('+'.$alpha.' seconds');
+										}
+										// parse here alphabetic value
 									}
-									// parse here alphabetic value
-								}
-								break;
+									break;
 
-							case 'year':
-								if (!empty($matches['digit'][0])) {
-									$year = $matches['digit'][0];
-									$datetime->modify('+'.$year.' year');
-									DebugStream::show('Add offset: +'.$year.' year'.PHP_EOL);
-								} else if ($allowAlphabeticUnits) {
-									$alpha = $language->translateUnit($matches['alpha'][0]);
-									if (is_numeric($alpha)) {
-										DebugStream::show('Add offset: +'.$alpha.' years'.PHP_EOL);
-										$datetime->modify('+'.$alpha.' years');
+								case 'year':
+									if (!empty($matches['digit'][0])) {
+										$year = $matches['digit'][0];
+										$datetime = $datetime->modify('+'.$year.' year');
+										DebugStream::show('Add offset: +'.$year.' year'.PHP_EOL);
+									} else if ($this->allowAlphabeticUnits) {
+										$alpha = $language->translateUnit($matches['alpha'][0]);
+										if (is_numeric($alpha)) {
+											DebugStream::show('Add offset: +'.$alpha.' years'.PHP_EOL);
+											$datetime = $datetime->modify('+'.$alpha.' year');
+										}
+										// parse here alphabetic value
 									}
-									// parse here alphabetic value
-								}
-								break;
+									break;
 
-							case 'week':
-								if (!empty($matches['digit'][0])) {
-									$week = $matches['digit'][0];
-									$datetime->modify('+'.$week.' week');
-									DebugStream::show('Add offset: +'.$week.' week'.PHP_EOL);
-								} else if ($allowAlphabeticUnits) {
-									$alpha = $language->translateUnit($matches['alpha'][0]);
-									if (is_numeric($alpha)) {
-										DebugStream::show('Add offset: +'.$alpha.' weeks'.PHP_EOL);
-										$datetime->modify('+'.$alpha.' weeks');
+								case 'week':
+									if (!empty($matches['digit'][0])) {
+										$week = $matches['digit'][0];
+										$datetime = $datetime->modify('+'.$week.' week');
+										DebugStream::show('Add offset: +'.$week.' week'.PHP_EOL);
+									} else if ($this->allowAlphabeticUnits) {
+										$alpha = $language->translateUnit($matches['alpha'][0]);
+										if (is_numeric($alpha)) {
+											DebugStream::show('Add offset: +'.$alpha.' weeks'.PHP_EOL);
+											$datetime = $datetime->modify('+'.$alpha.' weeks');
+										}
+										// parse here alphabetic value
 									}
-									// parse here alphabetic value
-								}
-								break;
+									break;
 
-							case 'day':
-								if (!empty($matches['digit'][0])) {
-									$day = $matches['digit'][0];
-									$datetime->modify('+'.$day.' day');
-									DebugStream::show('Add offset: +'.$day.' day'.PHP_EOL);
-								} else if ($allowAlphabeticUnits) {
-									$alpha = $language->translateUnit($matches['alpha'][0]);
-									if (is_numeric($alpha)) {
-										DebugStream::show('Add offset: +'.$alpha.' days'.PHP_EOL);
-										$datetime->modify('+'.$alpha.' days');
+								case 'day':
+									if (!empty($matches['digit'][0])) {
+										$day = $matches['digit'][0];
+										$datetime = $datetime->modify('+'.$day.' day');
+										DebugStream::show('Add offset: +'.$day.' day'.PHP_EOL);
+									} else if ($this->allowAlphabeticUnits) {
+										$alpha = $language->translateUnit($matches['alpha'][0]);
+										if (is_numeric($alpha)) {
+											DebugStream::show('Add offset: +'.$alpha.' days'.PHP_EOL);
+											$datetime = $datetime->modify('+'.$alpha.' days');
+										}
+										// parse here alphabetic value
 									}
-									// parse here alphabetic value
-								}
-								break;
+									break;
 
-							case 'month':
-								if (!empty($matches['digit'][0])) {
-									$month = $matches['digit'][0];
-									$datetime->modify('+'.$month.' month');
-									DebugStream::show('Add offset: +'.$month.' month'.PHP_EOL);
-								} else if ($allowAlphabeticUnits) {
-									$alpha = $language->translateUnit($matches['alpha'][0]);
-									if (is_numeric($alpha)) {
-										DebugStream::show('Add offset: +'.$alpha.' months'.PHP_EOL);
-										$datetime->modify('+'.$alpha.' months');
+								case 'month':
+									if (!empty($matches['digit'][0])) {
+										$month = $matches['digit'][0];
+										$datetime = $datetime->modify('+'.$month.' month');
+										DebugStream::show('Add offset: +'.$month.' month'.PHP_EOL);
+									} else if ($this->allowAlphabeticUnits) {
+										$alpha = $language->translateUnit($matches['alpha'][0]);
+										if (is_numeric($alpha)) {
+											DebugStream::show('Add offset: +'.$alpha.' months'.PHP_EOL);
+											$datetime = $datetime->modify('+'.$alpha.' months');
+										}
+										// parse here alphabetic value
 									}
-									// parse here alphabetic value
-								}
-								break;
+									break;
+							}
 						}
 					}
 				}
 			}
 		}
+		if ($datetime === $current_datetime && $falseWhenNotChanged)
+			return false;
 		return $datetime;
+	}
+
+	static public function parseString($string, $languages = 'all', $allowAlphabeticUnits = false, $falseWhenNotChanged = false) {
+		static $parsers = array();
+		if (!isset($parser[is_array($languages) ? implode(',', $languages) : $languages])) {
+			$parsers[is_array($languages) ? implode(',', $languages) : $languages] = new self($languages);
+			if ($allowAlphabeticUnits) $parsers[is_array($languages) ? implode(',', $languages) : $languages]->allowAlphabeticUnits();
+		}
+		return $parsers[is_array($languages) ? implode(',', $languages) : $languages]->parse($string, $falseWhenNotChanged);
 	}
 
 	/**
@@ -289,7 +327,7 @@ class TimeParser {
 	static private function populateRules($language) {
 		$data = json_decode(file_get_contents(dirname(dirname(__FILE__)).DIRECTORY_SEPARATOR.'rules'.DIRECTORY_SEPARATOR.$language.'.json'), true);
 		if ($data === null) {
-			throw new \Exception(json_last_error());
+			throw new Exception(json_last_error());
 		}
 		return new Language($data['language'], $data['rules'], $data['week_days'], $data['pronouns'], $data['months'], $data['units']);
 	}
---
+++ tests/TimeParserTest.php
@@ -0,0 +1,69 @@
+<?php
+require_once __DIR__.'/../vendor/autoload.php';
+
+use wapmorgan\TimeParser\TimeParser;
+
+class TimeParseTest extends PHPUnit_Framework_TestCase {
+    public function setUp() {
+        // TimeParser::enableDebug();
+    }
+    /**
+     * @dataProvider data()
+     */
+    public function testIterative($language, $substrings) {
+        $parser = new TimeParser($language);
+
+        foreach ($substrings as $substring => $dateTimeModifyString) {
+            $expected_date = new DateTime();
+            if (is_string($dateTimeModifyString)) {
+                // var_dump($dateTimeModifyString);
+                $expected_date->modify($dateTimeModifyString);
+            } else {
+                $unix = strtotime($dateTimeModifyString[1]);
+                if ($dateTimeModifyString[0] == 'date') {
+                    $date = explode('.', date('d.m.Y', $unix));
+                    $expected_date->setDate($date[2], $date[1], $date[0]);
+                }
+            }
+            $actual_date = $parser->parse($substring);
+            $this->assertEquals($expected_date->format('r'), $actual_date->format('r'));
+        }
+    }
+
+    public function data() {
+        return array(
+            array('russian', array(
+                '15 декабря 1977 года' => '15 december 1977',
+                'в 15:12:13' => '15:12:13',
+                'в следующий понедельник' => array('date', 'next monday'),
+                'в следующем году' => '+1 year',
+                'в феврале' => 'february',
+                'через 15 часов' => '+15 hour',
+                'через 10 минут' => '+10 minutes',
+                'через 11 секунд' => '+11 seconds',
+                'через 5 лет' => '+5 years',
+                'через 2 недели' => '+2 weeks',
+                'через 1 день' => '+1 day',
+                'через 10 месяцев' => '+10 month',
+            )),
+            array('english', array(
+                '15 december 1977 year' => '15 december 1977',
+                'at 15:12:13' => '15:12:13',
+                'next monday' => array('date', 'next monday'),
+                'next year' => '+1 year',
+                'in february' => 'february',
+                'in 15 hours' => '+15 hour',
+                'in 10 minutes' => '+10 minutes',
+                'in 11 seconds' => '+11 seconds',
+                'in 5 years' => '+5 years',
+                'in 2 weeks' => '+2 weeks',
+                'in 1 day' => '+1 day',
+                'in 10 months' => '+10 month',
+            ))
+        );
+    }
+
+    public function testEmptyDateTime() {
+        $this->assertFalse(TimeParser::parseString(implode(null, range('a', 'z')), 'all', false, true));
+    }
+}
